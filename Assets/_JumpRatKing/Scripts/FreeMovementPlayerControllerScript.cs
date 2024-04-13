81fffef4f5a6a70a53c4ae1c155152c7ff589e44
Christian Deese <cdeesemail@gmail.com>
2024-04-12 23:47:02 -0500

    added code for Jumping and Attacking.
    lost custom Jump animation so it must be redone.

--- Assets/_JumpRatKing/Scripts/FreeMovementPlayerControllerScript.cs
+++ Assets/_JumpRatKing/Scripts/FreeMovementPlayerControllerScript.cs
@@ -11,24 +11,48 @@ public class FreeMovementPlayerControllerScript : MonoBehaviour
     //the speed of the Player.
     private float _speed;
 
-    //stores our Player's body.
-    private Rigidbody _body;
+    [Space]
 
     [SerializeField]
-    //stores a reference to the Camera.
-    private Camera _camera;
+    [Tooltip("Delay before Player can attack again.")]
+    //the amount of time before CanAttack() is invoked after attacking.
+    private float _attackDelay;
 
     [SerializeField]
-    //stores a reference to our Player's weapon
+    //stores a reference to our Player's weapon.
     private GameObject _meleeWeapon;
 
+
+
+    //stores a reference to the animator that animates this.
     private Animator _animator;
+
+    //stores a reference to our rigidbody.
+    private Rigidbody _body;
+
+    //stores true if this Player is already jumping.
+    private bool _jumping;
+
+    //stores true if this Player can attack again.
+    private bool _canAttack;
+
+    //sets animator boolean to false.
+    private void StopAttacking()
+    {
+        _animator.SetBool("isAttacking", false);
+    }
+    //sets _canAttack to be true.
+    private void CanAttack()
+    {
+        _canAttack = true;
+    }
 
     void Awake()
     {
-        //get our rigidbody.
+        //get our body.
         _body = GetComponent<Rigidbody>();
 
+        //get our animator.
         _animator = GetComponent<Animator>();
     }
 
@@ -36,7 +60,7 @@ public class FreeMovementPlayerControllerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CanAttack();
     }
 
     // Update is called once per frame
@@ -49,8 +73,38 @@ public class FreeMovementPlayerControllerScript : MonoBehaviour
         //forward/backward
         float zAxis = Input.GetAxisRaw("Player1Depth");
 
+        //if the E key is pressed: Attack.
+        if (Input.GetKeyDown(KeyCode.E) && _canAttack)
+        {
+            //begin attacking.
+            _animator.SetBool("isAttacking", true);
+            //disable attacking.
+            _canAttack = false;
 
-        UnityEngine.Debug.Log((xAxis + ", " + yAxis + ", " + zAxis));
+            //re-enables Player attacking after a delay.
+            Invoke("CanAttack", _attackDelay);
+        }
+        else
+            //stop attacking.
+            StopAttacking();
+
+        //if we are already jumping.
+        if(_jumping)
+        {
+            //prevent jump input from reaching animation controller.
+            yAxis = 0f;
+        }
+
+        //if we are trying to jump.
+        if(yAxis > 0.1f && !_jumping)
+        { 
+            //set jumping to true.
+            _jumping = true;
+        }
+
+        //if we are not currently jumping or falling.
+        if(_body.velocity.y < 0.1f && _body.velocity.y > -0.1f)
+            _jumping = false;
 
         //sets the animation xAxis parameter.
         _animator.SetFloat("xAxis", xAxis); 
@@ -58,5 +112,8 @@ public class FreeMovementPlayerControllerScript : MonoBehaviour
         _animator.SetFloat("yAxis", yAxis);
         //sets the animation zAxis parameter.
         _animator.SetFloat("zAxis", zAxis);
+
+
+
     }
 }
